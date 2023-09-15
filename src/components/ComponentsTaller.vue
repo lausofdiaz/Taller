297a126719b1d45bec39a7ec642f64a1e60987f4
lausofdiaz <144862180+lausofdiaz@users.noreply.github.com>
2023-09-14 23:02:44 -0500

    porcentaje
    porcentaje de la compra

--- src/components/ComponentsTaller.vue
+++ src/components/ComponentsTaller.vue
@@ -1,15 +1,46 @@
 <script setup lang="ts">
-import {ref,defineProps, defineEmits} from 'vue';
+import {ref,defineProps, defineEmits,computed} from 'vue';
 
 const props = defineProps({
   articulos:Array
 });
-
 const emit = defineEmits(['borrar'])
-
 function eliminar(index){
 emit('borrar',index)
 }
+
+const valorTotal = computed(()=>{
+  let suma = 0
+  props.articulos?.forEach((elemento)=>{
+    suma += elemento.cantidad*elemento.valor
+  })
+  return suma
+})
+
+const porcentaje = computed(()=>{
+  let descuento = 0
+
+  
+  if(valorTotal.value >= 60.000){
+    descuento = 0.05;
+  } else if (valorTotal.value >=120.000){
+    descuento = 0.10;
+  } else if(valorTotal.value >=240.000){
+    descuento = 0.15;
+  }
+
+
+
+
+
+  let totalConDescuento = valorTotal.value*descuento
+  return totalConDescuento
+})
+
+
+const totalApagar = computed(()=>{
+let totaltotal = valorTotal.value - porcentaje.value
+})
 </script>
 
 <template>
@@ -37,7 +68,9 @@ emit('borrar',index)
           </tr>
         </tbody>
       </table>
-      <h3>Valor total: </h3>
+      <h3>Valor total: ${{ valorTotal }} </h3>
+      <h3>Descuento: {{ porcentaje }}% </h3>
+      <h3>Total a pagar: ${{ totalApagar }} </h3>
 </template>
 <style scoped>
 table{
